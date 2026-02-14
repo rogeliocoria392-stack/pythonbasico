b33ae6c0f070fda13851b6cbad12c1502480b004
rogeliocoria392-stack <rogeliocoria392@gmail.com>
2026-02-13 16:13:29 -0800

    e

---
+++ sesion_03.py
@@ -0,0 +1,22 @@
+# resultado:
+    # martes
+    # miercoles
+    # jueves 
+    # viernes 
+    # martes
+    # miercoles
+    # jueves 
+    # viernes 
+    # martes
+    # miercoles
+    # jueves 
+    # viernes 
+
+i = 0
+mi_lista_2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+while i< 3:
+    i += 1
+ 
+    for d in mi_lista_2:
+        if d != "Lunes":
+           print(d)
